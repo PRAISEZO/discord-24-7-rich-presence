503e990ec9dec6f3bf2a36cd130eb239a65a160b
PRAISEZO <PraisezoPlayinghbg@gmail.com>
2025-09-24 18:28:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ const config = yaml.load(fs.readFileSync("./config.yml", "utf8"));
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
