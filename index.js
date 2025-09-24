f953d2d8bb5cad3d7b250fd94ae671ddba4b0887
PRAISEZO <PraisezoPlayinghbg@gmail.com>
2025-09-24 18:23:23 +0000

    Update index.js
    hiiii

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ const fs = require("fs");
 const yaml = require("js-yaml");
 const dotenv = require("dotenv");
 const config = yaml.load(fs.readFileSync("./config.yml", "utf8"));
-const port = 8080
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 dotenv.config();
 const client = new Client();
 
